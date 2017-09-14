91b0b4574cade143c8b5a8fe80c8a43bba522b98
ramiGA <ramisn3@gmail.com>
2017-09-13 23:35:29 -0400

    update action

--- src/reducers/head_reducer.js
+++ src/reducers/head_reducer.js
@@ -3,7 +3,7 @@ import { FETCH_HEAD } from '../actions';
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_HEAD:
-      return _.mapKeys(action.payload.data.articles);
+      return _.mapKeys(action.payload.data.articles, 'id');
     default:
       return state;
   }
