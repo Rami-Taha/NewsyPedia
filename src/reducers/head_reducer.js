779ddd7b3958cd287ce8210778b9d2b86088a3f1
ramiGA <ramisn3@gmail.com>
2017-09-13 23:31:07 -0400

    update action

--- src/reducers/head_reducer.js
+++ src/reducers/head_reducer.js
@@ -1,8 +1,9 @@
+import _ from 'lodash';
 import { FETCH_HEAD } from '../actions';
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_HEAD:
-      return [...action.payload.data.articles, ...state];
+      return _.mapKeys(action.payload.data.articles);
     default:
       return state;
   }
