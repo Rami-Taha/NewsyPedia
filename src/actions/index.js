b1ca8d0bcc6147483ae8c7f3c7822e9860e9f968
ramiGA <ramisn3@gmail.com>
2017-09-15 15:01:07 -0400

    https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_KEY = '366c7f50514546b6bda4a0edda450f99';
-const ROOT_URL = `http://beta.newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
-const ROOT_SOURCE = `http://beta.newsapi.org/v2/sources?country=us&apiKey=${API_KEY}`;
+const ROOT_URL = `https://beta.newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
+const ROOT_SOURCE = `https://beta.newsapi.org/v2/sources?country=us&apiKey=${API_KEY}`;
 const HEAD_URL = `https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=${API_KEY}`;
 
 export const FETCH_NEWS = 'FETCH_NEWS';
