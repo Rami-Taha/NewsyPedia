3582012cf6b6927b0705fbf9b66f0190a0ce03e8
ramiGA <ramisn3@gmail.com>
2017-09-16 00:12:19 -0400

    styles

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-// // const publicPath = path.join(__dirname, '..', 'public');
-// const port = process.env.PORT || 8080;
-//
-// app.use(express.static(__dirname));
-//
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'index.html'));
-// });
-// app.listen(port, () => {
-//   console.log('Server is up!');
-// });
-//"postinstall": "webpack -p",
-//"start": "node server.js",
-//
+const path = require('path');
+const express = require('express');
+const app = express();
+// const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+app.listen(port, () => {
+  console.log('Server is up!');
+});
