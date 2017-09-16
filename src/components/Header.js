2961e8af58bdd5a1b59b3443e31783ab51e9dd6e
ramiGA <ramisn3@gmail.com>
2017-09-16 02:45:38 -0400

    logD

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,62 +3,60 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 const Header = () => {
   return (
-    <div className="container-fluid">
-      <nav className="navbar navbar-inverse navbar-primary">
-        <div className="container">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle collapsed bg-primary"
-              data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Menu</span>
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-            </button>
-            <a className="navbar-brand" href="#">
-              <span className="" aria-hidden="true">
-                NewsyPedia
-              </span>
-            </a>
-          </div>
-          <div
-            className="collapse navbar-collapse"
-            id="bs-example-navbar-collapse-1"
+    <nav className="navbar navbar-inverse navbar-primary">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed bg-primary"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+            aria-expanded="false"
           >
-            <ul className="nav navbar-nav">
-              <li>
-                <Link className="btn btn-primary-sm" to="/">
-                  Today Headlines
-                </Link>
-              </li>
-              <li>
-                {' '}
-                <Link className="btn btn-primary-sm" to="/search">
-                  Search Top News
-                </Link>
-              </li>
-              <li>
-                <Link className="btn btn-primary-sm" to="/index">
-                  News Directory
-                </Link>
-              </li>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <a href="#">Sign In</a>
-              </li>
-              <li>
-                <a href="#">Account</a>
-              </li>
-            </ul>
-          </div>
+            <span className="sr-only">Menu</span>
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+          <a className="navbar-brand" href="#">
+            <span className="" aria-hidden="true">
+              NewsyPedia
+            </span>
+          </a>
         </div>
-      </nav>
-    </div>
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav">
+            <li>
+              <Link className="btn btn-primary-sm" to="/">
+                Today Headlines
+              </Link>
+            </li>
+            <li>
+              {' '}
+              <Link className="btn btn-primary-sm" to="/search">
+                Search Top News
+              </Link>
+            </li>
+            <li>
+              <Link className="btn btn-primary-sm" to="/index">
+                News Directory
+              </Link>
+            </li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#">Sign In</a>
+            </li>
+            <li>
+              <a href="#">Account</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
